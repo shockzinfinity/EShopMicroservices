2b01d8ae5cfeeba5ad462fcb0f824492c468c422
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-03 18:25:31 +0900

    finalize Catalog API CRUD operations

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -3,13 +3,16 @@
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
+  : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
   public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
   {
     // create Product entity from command object
     // save to database
     // return CreateProductResult result
+
+    logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
 
     var product = new Product
     {
