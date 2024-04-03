994b016e6c94536b870c4a3b47b4ede174ff611e
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-03 14:47:57 +0900

    modify cqrs

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,28 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-  public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+  public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
   {
-    // Business logic to create a product
-    throw new NotImplementedException();
+    // create Product entity from command object
+    // save to database
+    // return CreateProductResult result
+
+    var product = new Product
+    {
+      Name = command.Name,
+      Category = command.Category,
+      Description = command.Description,
+      ImageFile = command.ImageFile,
+      Price = command.Price
+    };
+
+    return new CreateProductResult(Guid.NewGuid());
   }
 }
