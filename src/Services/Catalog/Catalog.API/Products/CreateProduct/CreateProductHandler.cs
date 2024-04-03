6eb5f88c87df3f5eb3773b6e309d418575a20f92
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-03 18:52:01 +0900

    fluent validation test

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,9 +1,21 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+  : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+  public CreateProductCommandValidator()
+  {
+    RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+    RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+    RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+    RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+  }
+}
+
+internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger, IValidator<CreateProductCommand> validator)
   : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
   public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -11,6 +23,13 @@ internal class CreateProductCommandHandler(IDocumentSession session, ILogger<Cre
     // create Product entity from command object
     // save to database
     // return CreateProductResult result
+
+    var result = await validator.ValidateAsync(command, cancellationToken);
+    var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+    if (errors.Any())
+    {
+      throw new ValidationException(errors.FirstOrDefault());
+    }
 
     logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
 
