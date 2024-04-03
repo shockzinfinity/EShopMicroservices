613f605f4b309d04da9f924e030152206e6be25b
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-03 19:53:28 +0900

    custom exception handler

--- src/Services/Catalog/Catalog.API/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -1,8 +1,11 @@
 ﻿global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
 global using Carter;
 global using Catalog.API.Exceptions;
 global using Catalog.API.Models;
+global using FluentValidation;
 global using Mapster;
 global using Marten;
 global using MediatR;
-global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.API.Products.GetProductById;
 
-public record GetProductByIdQuery(Guid id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
 public record GetProductByIdResult(Product Product);
 
 internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
@@ -10,11 +10,11 @@ internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetP
   {
     logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
 
-    var product = await session.LoadAsync<Product>(query.id, cancellationToken);
+    var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
 
     if (product is null)
     {
-      throw new ProductNotFoundException();
+      throw new ProductNotFoundException(query.Id);
     }
 
     return new GetProductByIdResult(product);
