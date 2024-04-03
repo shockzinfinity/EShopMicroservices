855721c9e144940a45adf8632a4d5f5d4420fcc0
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-03 19:14:49 +0900

    add validation rules

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -3,6 +3,18 @@
 public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
 public record UpdateProductResult(bool IsSuccess);
 
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+  public UpdateProductCommandValidator()
+  {
+    RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+    RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required").Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+    //RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+    //RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+    RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+  }
+}
+
 internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
   : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
