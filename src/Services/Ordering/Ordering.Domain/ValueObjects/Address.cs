4c6afd41449cd13a4c3914afa6b4862258e77e2f
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-04 18:33:52 +0900

    rich domain model implementation

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -10,4 +10,25 @@ public record Address
   public string State { get; } = default!;
   public string City { get; } = default!;
   public string ZipCode { get; } = default!;
+
+  protected Address() { }
+  private Address(string firstName, string lastName, string? emailAddress, string addressLine, string country, string state, string city, string zipCode)
+  {
+    FirstName = firstName;
+    LastName = lastName;
+    EmailAddress = emailAddress;
+    AddressLine = addressLine;
+    Country = country;
+    State = state;
+    City = city;
+    ZipCode = zipCode;
+  }
+
+  public static Address Of(string firstName, string lastName, string? emailAddress, string addressLine, string country, string state, string city, string zipCode)
+  {
+    ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+    ArgumentException.ThrowIfNullOrWhiteSpace(addressLine);
+
+    return new Address(firstName, lastName,  ? emailAddress, addressLine, country, state, city, zipCode);
+  }
 }
