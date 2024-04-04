77b52ef881e4b0e00f8c080a6a4f82b1627ceb2e
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-04 11:26:12 +0900

    add health checks

--- src/Services/Basket/Basket.API/GlobalUsings.cs
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -7,6 +7,8 @@ global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Exceptions.Handler;
 global using Carter;
 global using FluentValidation;
+global using HealthChecks.UI.Client;
 global using Mapster;
 global using Marten;
 global using MediatR;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
