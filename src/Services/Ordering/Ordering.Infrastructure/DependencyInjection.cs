772392de5d874aee6bda179ef32dacd7d31867c0
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-04 19:47:45 +0900

    dbcontext settings

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -9,6 +9,8 @@ public static class DependencyInjection
   {
     var connectionString = configuration.GetConnectionString("Database");
 
+    services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
     return services;
   }
 }
---
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Infrastructure.Data;
