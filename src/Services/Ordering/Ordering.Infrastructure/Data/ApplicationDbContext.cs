b44a8f0e3f284d50bd252d992da2f0707062d085
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-04 21:31:15 +0900

    add migrations

--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -4,6 +4,13 @@ using System.Reflection;
 
 namespace Ordering.Infrastructure.Data;
 
+// Migration 생성 시
+// Ordering.API 프로젝트에 Design 패키지 설치 되어 있어야 하고,
+// Ordering.Infrastructure 위치에서 아래와 같이 실행
+// ef-command: dotnet ef migrations add InitialCreate -o Data\Migrations -s ..\Ordering.API\Ordering.API.csproj
+// 혹은
+// 패키지 관리자 콘솔에서는 시작 프로젝트를 Infrastructure 로 지정하고 아래로 실행
+// ps-command: Add-Migration InitialCreate -OutputDir Data/Migrations -Project Ordering.Infrastructure -StartupProject Ordering.API
 public class ApplicationDbContext : DbContext
 {
   public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
