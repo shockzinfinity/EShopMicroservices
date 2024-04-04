3563964133c745a1e03a4cf67e332bd6d8a02df0
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-04 09:54:05 +0900

    add basket project

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+  config.RegisterServicesFromAssembly(assembly);
+  config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+  config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+
+app.Run();
