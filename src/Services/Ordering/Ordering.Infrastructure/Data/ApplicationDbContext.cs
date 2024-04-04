0e976dc705f47ff62e9e4aa7213c1631bc638fa7
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-04 23:37:27 +0900

    save temp

--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -6,9 +6,11 @@ namespace Ordering.Infrastructure.Data;
 // Ordering.API 프로젝트에 Design 패키지 설치 되어 있어야 하고,
 // Ordering.Infrastructure 위치에서 아래와 같이 실행
 // ef-command: dotnet ef migrations add InitialCreate -o Data\Migrations -s ..\Ordering.API\Ordering.API.csproj
+//             dotnet ef database update -s ..\Ordering.API\Ordering.API.csproj
 // 혹은
 // 패키지 관리자 콘솔에서는 시작 프로젝트를 Infrastructure 로 지정하고 아래로 실행
 // ps-command: Add-Migration InitialCreate -OutputDir Data/Migrations -Project Ordering.Infrastructure -StartupProject Ordering.API
+//             Update-Database
 public class ApplicationDbContext : DbContext
 {
   public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Ordering.Domain.Abstractions;
+
+namespace Ordering.Infrastructure.Data.Interceptors;
+
+public class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+  public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+  {
+    UpdateEntities(eventData.Context);
+    return base.SavingChanges(eventData, result);
+  }
+
+  public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+  {
+    UpdateEntities(eventData.Context);
+    return base.SavingChangesAsync(eventData, result, cancellationToken);
+  }
+
+  public void UpdateEntities(DbContext? context)
+  {
+    if (context == null) return;
+
+    foreach (var entry in context.ChangeTracker.Entries<IEntity>())
+    {
+    }
+  }
+}
