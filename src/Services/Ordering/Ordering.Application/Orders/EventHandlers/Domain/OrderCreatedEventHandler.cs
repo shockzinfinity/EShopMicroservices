838fe77722fa8f887c761dd3c1ae0c9304c1c47e
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-05 16:55:06 +0900

    add integration event (create order)
    but it will raise when app starts. (by seeding database).
    so we need a asp.net feature management

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,11 +1,16 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
+﻿using MassTransit;
 
-public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderCreatedEventHandler(IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger)
   : INotificationHandler<OrderCreatedEvent>
 {
-  public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+  public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
   {
-    logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
-    return Task.CompletedTask;
+    logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+    var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
+
+    await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
   }
 }
