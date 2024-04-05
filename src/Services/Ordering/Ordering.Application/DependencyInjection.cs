7dd5133bae5557ef519673f40c5eb6f739e1cebc
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-05 10:37:54 +0900

    ordering application

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -7,7 +7,7 @@ public static class DependencyInjection
 {
   public static IServiceCollection AddApplicationServices(this IServiceCollection services)
   {
-    // 여기에서 MediatR 이 container 에 등록되므로, infrastructure layer 에서 별도 참조 추가를 하지 않고 사용이 가능함
+    // 여기에서 MediatR 이 container 에 등록되므로, infrastructure layer 에서 별도 injection 하지 않아도 된다.
     services.AddMediatR(cfg =>
     {
       cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
---
+++ src/Services/Ordering/Ordering.Application/Dtos/PaymentDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Ordering.Application.Dtos;
+
+// Mapster 에서 전부 대문자인 경우 레코드 타입에서 제대로 매핑되지 않음 (Cvv)
+public record PaymentDto(string CardName, string CardNumber, string Expiration, string Cvv, int Paymethod);
