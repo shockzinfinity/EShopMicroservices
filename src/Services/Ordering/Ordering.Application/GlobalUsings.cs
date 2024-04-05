cfb96481f2b1dd55244e04eeff73b4d797e0bbb2
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-05 12:25:46 +0900

    get orders with pagination

--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -1,4 +1,5 @@
 ﻿global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Pagination;
 global using FluentValidation;
 global using MediatR;
 global using Microsoft.EntityFrameworkCore;
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,4 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersResult>;
+public record GetOrdersResult(PaginatedResult<OrderDto> Orders);
