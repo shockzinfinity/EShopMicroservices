3133a662fc9b747ed0025ccea44c364278acc94d
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-06 19:15:24 +0900

    add rate limiting

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+  options.AddFixedWindowLimiter("fixed", opt =>
+  {
+    opt.Window = TimeSpan.FromSeconds(10);
+    opt.PermitLimit = 5;
+  });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 app.Run();
