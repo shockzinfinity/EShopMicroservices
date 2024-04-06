e8cc1a48df4a9f4381e93f6f363d2793ab5b8f7f
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-06 19:59:44 +0900

    add models

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Shopping.Web.Pages
@@ -14,7 +13,6 @@ namespace Shopping.Web.Pages
 
     public void OnGet()
     {
-
     }
   }
 }
