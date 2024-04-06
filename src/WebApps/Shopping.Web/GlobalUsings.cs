6b0a20af114d8fb10966a7dacb4746dcf15f035d
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-06 20:22:20 +0900

    add services

--- src/WebApps/Shopping.Web/GlobalUsings.cs
+++ src/WebApps/Shopping.Web/GlobalUsings.cs
@@ -1,1 +1,5 @@
-﻿global using Shopping.Web.Models.Catalog;
+﻿global using Refit;
+global using Shopping.Web.Models.Basket;
+global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Ordering;
+global using Shopping.Web.Services;
--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -2,6 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddRefitClient<ICatalogService>().ConfigureHttpClient(c =>
+{
+  c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+});
 
 var app = builder.Build();
 
