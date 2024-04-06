a5dd076d04d3cb5fba635a3846c53488af7f534e
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-04-06 21:12:31 +0900

    update layout and add to cart page

---
+++ src/WebApps/Shopping.Web/Pages/ProductList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shopping.Web.Pages
+{
+    public class ProductListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- src/WebApps/Shopping.Web/Services/IBasketService.cs
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -13,4 +13,27 @@ public interface IBasketService
 
   [Post("/basket-service/basket/checkout")]
   Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+
+  public async Task<ShoppingCartModel> LoadUserBasket()
+  {
+    // get basket if not exist create new basket with default logged in user name.
+    var userName = "swn";
+    ShoppingCartModel basket;
+
+    try
+    {
+      var getBasketResponse = await GetBasket(userName);
+      basket = getBasketResponse.Cart;
+    }
+    catch (ApiException apiException) when (apiException.StatusCode == System.Net.HttpStatusCode.NotFound)
+    {
+      basket = new ShoppingCartModel
+      {
+        UserName = userName,
+        Items = []
+      };
+    }
+
+    return basket;
+  }
 }
